a5b887130539a3463a39e4cf13a9dbfd9ca11a54
Patrick Bosman <bsmn.ptrck@gmail.com>
2019-08-30 16:58:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ function takeANumber (line, name) {
   line.push(name);
   return `Welcome, ${name}. You are number ${line.length} in line.`
 }
+function takeANumber (line) {
+  var pos = 0
+  pos++
+  return `Welcome, you have ticket number ${pos}`
+  line.push(pos)
+}
+
+
 function nowServing (line) {
   if (line.length === 0) {
     return "There is nobody waiting to be served!"
